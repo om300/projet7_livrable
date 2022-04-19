a0929067d7e5692df2c445ca4cf2018b6fc079a2
om300 <43752393+om300@users.noreply.github.com>
2022-04-19 11:51:54 +0200

    Update DASHBOARD_Projet7.py

--- DASHBOARD_Projet7.py
+++ DASHBOARD_Projet7.py
@@ -91,55 +91,55 @@ def load_data():
 
 # near real-time / live feed simulation 
 
-'''for seconds in range(2306):
+#for seconds in range(2306):
 #while True: 
     
-    df['Age'] = df['Age'] 
-    df['balance_new'] = df['AMT_CREDIT'] 
+    #df['Age'] = df['Age'] 
+    #df['balance_new'] = df['AMT_CREDIT'] 
 
     # creating KPIs 
-    avg_age = np.mean(df['Age']) 
+    #avg_age = np.mean(df['Age']) 
 
-    count_married = int(df[(df["Married"]=='Yes')]['Married'].count())
+    #count_married = int(df[(df["Married"]=='Yes')]['Married'].count())
     
-    balance = np.mean(df['AMT_CREDIT'])
+    #balance = np.mean(df['AMT_CREDIT'])
     
-    AMT_INCOME_TOTAL = np.mean(df['AMT_INCOME_TOTAL'])
+    #AMT_INCOME_TOTAL = np.mean(df['AMT_INCOME_TOTAL'])
     
-    AMT_GOODS_PRICE = np.mean(df['AMT_GOODS_PRICE'])
+    #AMT_GOODS_PRICE = np.mean(df['AMT_GOODS_PRICE'])
     
     #AMT_ANNUITY: Loan annuity / regular EMI to the bank
-    AMT_ANNUITY = np.mean(df['AMT_ANNUITY'])
+    #AMT_ANNUITY = np.mean(df['AMT_ANNUITY'])
 
 
-    with placeholder.container():
+    #with placeholder.container():
         # create three columns
-        kpi1, kpi2, kpi3 ,kpi4, kpi5, kpi6 = st.columns(6)
+        #kpi1, kpi2, kpi3 ,kpi4, kpi5, kpi6 = st.columns(6)
 
         # fill in those three columns with respective metrics or KPIs 
       # fill in those three columns with respective metrics or KPIs 
-        kpi1.metric(label="Age ⏳", value=round(avg_age))
-        kpi2.metric(label="Married Count 💍", value= int(count_married))
-        kpi3.metric(label="Amount of credit ＄", value= f"$ {round(balance,2)} ")
-        kpi4.metric(label="Amount Income ＄", value= int(AMT_INCOME_TOTAL))
+        #kpi1.metric(label="Age ⏳", value=round(avg_age))
+        #kpi2.metric(label="Married Count 💍", value= int(count_married))
+        #kpi3.metric(label="Amount of credit ＄", value= f"$ {round(balance,2)} ")
+        #kpi4.metric(label="Amount Income ＄", value= int(AMT_INCOME_TOTAL))
         #For consumer loans it is the price of the goods for which the loan is given 
-        kpi5.metric(label="AMT_GOODS_PRICE ＄", value= int(AMT_GOODS_PRICE))
+        #kpi5.metric(label="AMT_GOODS_PRICE ＄", value= int(AMT_GOODS_PRICE))
         #AMT_ANNUITY: Loan annuity / regular EMI to the bank
-        kpi6.metric(label="AMT_ANNUITY ⏳", value= int(AMT_ANNUITY))
+        #kpi6.metric(label="AMT_ANNUITY ⏳", value= int(AMT_ANNUITY))
 
 
         
         # create two columns for charts 
 
-        fig_col1, fig_col2 = st.columns(2)
-        with fig_col1:
-            st.markdown("### First Chart")
-            fig = px.density_heatmap(data_frame=df, y = 'Age', x = 'Married')
-            st.write(fig)
-        with fig_col2:
-            st.markdown("### Second Chart")
-            fig2 = px.histogram(data_frame = df, x = 'Age')
-            st.write(fig2)
+        #fig_col1, fig_col2 = st.columns(2)
+        #with fig_col1:
+            #st.markdown("### First Chart")
+            #fig = px.density_heatmap(data_frame=df, y = 'Age', x = 'Married')
+            #st.write(fig)
+        #with fig_col2:
+            #st.markdown("### Second Chart")
+            #fig2 = px.histogram(data_frame = df, x = 'Age')
+            #st.write(fig2)
         #st.markdown("### Detailed Data View")
         #st.dataframe(df)
         #time.sleep(20)
